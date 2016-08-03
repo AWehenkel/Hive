0571488568a7147d0210b1a78e64865cef632945
AWehenkel <antoine.wehenkel@gmail.com>
2016-08-03 10:20:57 +0200

    First commit

--- AddPv.py
+++ AddPv.py
@@ -13,27 +13,32 @@ db = MySQLdb.connect("localhost","root","videogame2809","hive" )
 
 fn = 'pv_puiss2.xlsx'
 data = open_workbook('data/' + fn)
-request = 'INSERT INTO pv (position, power) VALUES '
-s = data.sheet_by_index(0)
-for i in range(1, 200):
-    street = str(s.col(5)[i].value.encode('ascii', 'ignore'))
-    power = s.col(4)[i].value
-    geocode_result = gmaps.geocode(street )
-    if(geocode_result):
-        lat = geocode_result[0]['geometry']['location']['lat']
-        long = geocode_result[0]['geometry']['location']['lng']
-        add = str(lat) + ',' + str(long)
-        print i
-        request += '(\'' + add + '\', \'' + str(power) + '\'),'
 
-cur = db.cursor()
-request = request[0:request.__len__() - 1]
-try:
-    # Execute the SQL command
-    cur.execute(request)
-    # Commit your changes in the database
-    db.commit()
-    print "donnees enregistrees. \n"
-except:
-    # Rollback in case there is any error
-    db.rollback()
+s = data.sheet_by_index(0)
+print s.nrows
+for i in range(2, s.nrows//300):
+    request = 'INSERT INTO pv (position, power) VALUES '
+    print i
+    raw_input("pause")
+    for j in range(300*i, 300*(i+1)):
+        street = str(s.col(5)[j].value.encode('ascii', 'ignore'))
+        power = s.col(4)[j].value
+        geocode_result = gmaps.geocode(street )
+        if(geocode_result):
+            lat = geocode_result[0]['geometry']['location']['lat']
+            long = geocode_result[0]['geometry']['location']['lng']
+            add = str(lat) + ',' + str(long)
+            print j
+            request += '(\'' + add + '\', \'' + str(power) + '\'),'
+
+    cur = db.cursor()
+    request = request[0:request.__len__() - 1]
+    try:
+        # Execute the SQL command
+        cur.execute(request)
+        # Commit your changes in the database
+        db.commit()
+        print "donnees enregistrees. \n"
+    except:
+        # Rollback in case there is any error
+        db.rollback()
