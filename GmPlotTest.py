89a0220e24eacfd21da2d88c85ea2b8ecb6590ef
antoi <antoine-dubois@hotmail.com>
2016-08-04 13:29:03 +0200

    04-08-16

--- GmPlotTest.py
+++ GmPlotTest.py
@@ -3,7 +3,6 @@ import googlemaps
 
 gmaps = googlemaps.Client(key='AIzaSyBj7JAQHEc-eFQkfuCXBba0dItAUPL0fMI')
 
-
 gmap = gmplot.GoogleMapPlotter(37.428, -122.145, 16)
 
 gmap.plot(37.427, -122.145, 'cornfolwerblue', edge_width=10)
