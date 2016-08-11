bda713bb8786c570ce3c5df8fed0fd9ad3cae4f9
antoi <antoine-dubois@hotmail.com>
2016-08-11 14:05:22 +0200

    11-08-16

--- StationsOnTheGo.py
+++ StationsOnTheGo.py
@@ -24,7 +24,6 @@ class StationsOnTheGo:
         except:
             print "Unable to fetch data"
 
-
         #Draw the two points on the map
         mymap = gmplot.GoogleMapPlotter(x[0], x[1], 8)
         mymap.marker(float(x[0]), float(x[1]),title="PointA", c="#00FF00")
